d9ea8aa1a13828c72bf4d102ae41eff0fae054af
Manideep Vemula <deep05739@gmail.com>
2025-03-20 11:38:52 +0530

    unnecessary code

--- ShoppingCartAPI/Service/CouponService.cs
+++ ShoppingCartAPI/Service/CouponService.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
 using Organic.Services.ShoppingCartAPI.Models.Dto;
 using ShoppingCartAPI.Service.IService;
 
@@ -9,21 +7,14 @@ namespace ShoppingCartAPI.Service
 	public class CouponService : ICouponService
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
-		private readonly IHttpContextAccessor _contextAccessor;
-
-		public CouponService(IHttpClientFactory clientFactory, IHttpContextAccessor contextAccessor)
+		public CouponService(IHttpClientFactory clientFactory)
 		{
 			_httpClientFactory = clientFactory;
-			_contextAccessor = contextAccessor;
 		}
 
 		public async Task<CouponDto> GetCoupon(string couponCode)
 		{
 			var client = _httpClientFactory.CreateClient("Coupon");
-			if (_contextAccessor.HttpContext?.Request.Cookies.TryGetValue("JWTToken", out var token) == true)
-			{
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-			}
 			var response = await client.GetAsync($"/api/CouponAPI/GetByCode/{couponCode}");
 			var apiContet = await response.Content.ReadAsStringAsync();
 			var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContet);
